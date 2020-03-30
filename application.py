8a2df3770c91261a0ba897a81e766e7d5d0cae99
Ivan252512 <ivanpineda@ciencias.unam.mx>
2020-03-30 03:18:44 -0600

    aws

--- application.py
+++ application.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template, request, jsonify, Response, redirect, make_response
+from flask import Flask, render_template, request, jsonify, Response, redirect, make_response, send_file
 from flask_pymongo import pymongo
 from bson.json_util import dumps
-from werkzeug.utils import secure_filename
 import os
 import pandas as pd
 
-from google.cloud import storage
 from openpyxl import load_workbook
-
+import logging
+import boto3
+from botocore.exceptions import ClientError
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
 # Configure this environment variable via app.yaml
-CLOUD_STORAGE_BUCKET = "liverpool-excel.appspot.com"
-#os.environ['CLOUD_STORAGE_BUCKET']
+AWS_BUCKET_NAME = "liverpoolexcel"
+UPLOAD_FOLDER = "media"
 
 ALLOWED_EXTENSIONS = {'xlsx', 'xls'}
 
@@ -22,17 +22,18 @@ client = pymongo.MongoClient(CONNECTION_STRING, maxPoolSize=10000)
 db = client.get_database('excel')
 documents_collection = pymongo.collection.Collection(db, 'documents')
 
-class Excel():
-    def __init__(self, name, url=None):
-        self.url = url
+class Excel:
+    def __init__(self, name):
         self.name = name
         try:
-            self.df = pd.read_excel(url, None)
-        except ValueError:
-            self.df = pd.read_excel("CUENTAS_FUN_SOCIEDADES.xlsx", None)
+            self.df = pd.read_excel("https://liverpoolexcel.s3-us-west-1.amazonaws.com/media/"+name, None)
+        except:
+            self.df = pd.read_excel("Seleccionar.xlsx", None)
         finally:
             self.sheets = self.df.keys()
 
+    def get_sheets(self):
+        return self.df.keys()
 
     def load_to_db(self):
         sheets = {}
@@ -42,7 +43,6 @@ class Excel():
             sheets[s] = i
             k+=1
         documents_collection.insert_one({"name": self.name , 
-                                         "url": self.url,
                                          "sheets": sheets})
     def get_sheets(self):
         return self.sheets
@@ -69,9 +69,11 @@ class Excel():
             return df, 200
         return df, 500
 
-
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def upload_file(file_name, bucket):
+    object_name = file_name
+    s3_client = boto3.client('s3')
+    response = s3_client.upload_file(file_name, bucket, object_name)
+    return response
 
 @app.route("/", methods=['GET'])
 def home():
@@ -85,16 +87,16 @@ def tables(document, sheet):
     documents2 = documents_collection.find({})
 
     document_db = documents_collection.find_one({"name":document})
+    
 
-    try:
-        sheets = list(document_db["sheets"].values())
-        excel = Excel(document_db["name"], document_db["url"])
-        if sheet == "CUENTAS" or sheet == "Seleccionar":
-            sheet = sheets[0]
-    except:
+    if document_db==None or len(document_db)<1:
         excel = Excel(document)
         sheets = excel.get_sheets()
-
+    else:
+        sheets = list(document_db["sheets"].values())
+        excel = Excel(document_db["name"])
+        if sheet == "Seleccionar":
+            sheet = sheets[0]
     
     doc, res = excel.filter(sheet,  1)
 
@@ -127,7 +129,7 @@ def tables_filter(document, sheet):
     content = request.get_json()
     document_db = documents_collection.find_one({"name":document})
 
-    excel = Excel(document_db["name"], document_db["url"])
+    excel = Excel(document_db["name"])
     doc, res = excel.filter(str(sheet),  content)
 
     cols_values = []
@@ -144,43 +146,22 @@ def tables_filter(document, sheet):
 @app.route('/load_database', methods=['POST'])
 def load_database():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect('tables')
-        file = request.files['file']
-        if file.filename == '':
-            return redirect('tables')
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-
-            # Create a Cloud Storage client.
-            gcs = storage.Client()
-
-            # Get the bucket that the file will be uploaded to.
-            bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
-
-            # Create a new blob and upload the file's content.
-            blob = bucket.blob(filename)
-
-            blob.upload_from_string(
-                file.read(),
-                content_type=file.content_type
-            )
-
-            excel = Excel(filename, blob.public_url)
-            excel.load_to_db()
-        return redirect("/tables/CUENTAS_FUN_SOCIEDADES.xlsx/CUENTAS")
+        f = request.files['file']
+        f.save(os.path.join(UPLOAD_FOLDER, f.filename))
+        upload_file(f"media/{f.filename}", AWS_BUCKET_NAME)
+        os.remove(f"media/{f.filename}")
+        excel = Excel(f.filename)
+        excel.load_to_db()
+    return redirect("/tables/Seleccionar.xlsx/Seleccionar")
 
 @app.route('/drop_database', methods=['GET'])
 def drop_database():
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
-    blobs = bucket.list_blobs()
-    for blob in blobs:
-        blob.delete()
-
+    s3 = boto3.resource('s3')
+    for i in documents_collection.find({}):
+        s3.Object(AWS_BUCKET_NAME, i["name"]).delete()
     documents_collection.drop()
-
-    return redirect("/tables/CUENTAS_FUN_SOCIEDADES.xlsx/CUENTAS")
+    return redirect("/tables/Seleccionar.xlsx/Seleccionar")
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
